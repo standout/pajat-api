1b7178dde93afa226c009c29adaa2091608fa7a7
David Elbe <david@standout.se>
2015-12-18 13:48:32 +0100

    Add report creation support

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,23 @@
 class ReportsController < ApplicationController
+
+  def create
+    @report = Report.new report_create_params
+    if @report.save
+      render json: @report
+    else
+      render json: @report, status: 422
+    end
+  end
+
+  protected
+
+  def report_create_params
+    params
+      .require(:report)
+      .permit(
+        :latitude,
+        :longitude,
+        :description
+      )
+  end
 end
