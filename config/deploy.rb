2269c5b3d740ae781cca2a2fb62b601aad998fa9
Alexander Ross <alex@aross.se>
2015-12-18 16:52:22 +0100

    Fix rbenv prefix in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}_#{fetch(:stage)}"
 set :rbenv_ruby, "#{File.read('.ruby-version').chomp}"
 set :rbenv_type, :user
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
 # Default value for :scm is :git
 # set :scm, :git
