2847a554fdac1b11a8210dbd7e4966834d0d2d99
David Elbe <david@standout.se>
2015-12-18 14:49:37 +0100

    Add more attributes to Report (agent, reporter etc)

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -4,14 +4,22 @@ RSpec.describe ReportsController, type: :controller do
   describe 'creation' do
     it 'should be created' do
       expect do
-        post "create",
-             report: {
-               latitude: 18,
-               longitude: 20,
-               description: 'test'
-             }
+        post "create", report: valid_create_params
       end.to change { Report.count }
       expect(response).to be_successful
     end
+  end
+
+  def valid_create_params
+    {
+      latitude: 18,
+      longitude: 20,
+      description: 'test',
+      agent: 'testbot 0.1.2',
+      reporter_network: 'twitter',
+      reporter_username: 'davidelbe',
+      source_url: 'https://twitter.com/azazell0/status/113728077904871424',
+      image_url: 'https://example.com/1723640659/chimp_bigger.png'
+    }
   end
 end
