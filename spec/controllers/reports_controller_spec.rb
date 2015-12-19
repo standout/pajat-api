7f4563c0fb07c63e632cd29bb5a35f0c23f0050d
Albert Arvidsson <albert.arvidsson@gmail.com>
2015-12-19 13:18:17 +0100

    Refactor reports_controller_spec to use factories

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe ReportsController, type: :controller do
 
   describe 'listing' do
     before(:context) do
-      40.times { Report.create! valid_create_params }
+      40.times { Report.create! attributes_for(:report) }
     end
 
     it 'responds successfully' do
@@ -33,24 +33,11 @@ RSpec.describe ReportsController, type: :controller do
     end
   end
 
-  def valid_create_params
-    {
-      latitude: 18,
-      longitude: 20,
-      description: 'test',
-      agent: 'testbot 0.1.2',
-      reporter_network: 'twitter',
-      reporter_username: 'davidelbe',
-      source_url: 'https://twitter.com/azazell0/status/113728077904871424',
-      image_url: 'https://example.com/1723640659/chimp_bigger.png'
-    }
-  end
-
   def list_reports
     get :index
   end
 
   def create_report
-    post "create", report: valid_create_params
+    post :create, report: attributes_for(:report)
   end
 end
